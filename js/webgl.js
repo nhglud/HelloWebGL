0b24de7c865de330aebb5bab80e6ee137f607994
nhglud <nhglud@gmail.com>
2025-05-01 09:41:00 +0200

    tidying

--- js/webgl.js
+++ js/webgl.js
@@ -553,12 +553,21 @@ function SetTextureFilters(image) {
         gl.generateMipmap(gl.TEXTURE_2D);
     }
     else {
-        gl.texParameteri(gl.TEXTURE_2D,
-        gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D,
-        gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D,
-        gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(
+            gl.TEXTURE_2D,
+            gl.TEXTURE_WRAP_S, 
+            gl.CLAMP_TO_EDGE
+        );
+        gl.texParameteri(
+            gl.TEXTURE_2D,
+            gl.TEXTURE_WRAP_T,
+            gl.CLAMP_TO_EDGE
+        );
+        gl.texParameteri(
+            gl.TEXTURE_2D,
+            gl.TEXTURE_MIN_FILTER, 
+            gl.LINEAR
+        );
     }
 }
 
